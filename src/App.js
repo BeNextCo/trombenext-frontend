b5cc0d52c1bf5ae1d9236a0b8a33a373a04940b6
Alezco <benjaminmorali4@hotmail.fr>
2019-08-07 14:32:58 +0200

    Add link to backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [helloWorld, setHelloWorld] = useState('Loading...')
+
+  useEffect(() => {
+    axios.get('http://localhost:5000').then(result => setHelloWorld(result.data))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +17,9 @@ function App() {
         <p>
           Trombenext
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          {helloWorld}
+        </p>
       </header>
     </div>
   );
