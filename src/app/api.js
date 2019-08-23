9165b0e6eec1e45243116408ec624ce30a8bfe9d
Baptiste Doucerain <doucerain.b@gmail.com>
2019-08-23 09:44:45 +0200

    Remove var

--- src/app/api.js
+++ src/app/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-var axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   header: { "Content-Type": "application/json" }
 });
